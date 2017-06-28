0a353f607b3ceba21dd7192794b26317d94560b8
Gunjan Sethi <gsethi2409@gmail.com>
2017-06-28 18:38:44 +0530

    HardReset.ino: Add File

---
+++ HardReset.ino
@@ -0,0 +1,13 @@
+bool resetLora() {
+  debugPrintln("Hard reseting");
+  pinMode(LORA_RESET, OUTPUT);
+  digitalWrite(LORA_RESET, HIGH);
+  sodaq_wdt_safe_delay(100);
+  
+  
+  digitalWrite(LORA_RESET, LOW);
+  sodaq_wdt_safe_delay(100);
+  digitalWrite(LORA_RESET, HIGH);
+  sodaq_wdt_safe_delay(1000);
+  initLora();
+}
